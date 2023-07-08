fadda8e98f4de3991c73b93119a4a9e8394280f1
huongcucnguyen <huongcucnguyen0889@gmail.com>
2023-07-08 17:33:17 +0700

    first commit

---
+++ app/functions/thread_get.php
@@ -0,0 +1,14 @@
+<?php
+$thread_array = array();
+
+//コメントデータをテーブルから取得してくる。
+$sql = "SELECT * FROM thread";
+$statement = $pdo ->prepare($sql);
+$statement->execute();
+
+$thread_array = $statement;
+
+
+
+
+
